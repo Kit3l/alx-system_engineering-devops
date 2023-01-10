064553471afd09efc01bde73211cebcd2a9f3dbe
Kit3l <pchumbakitel@gmail.com>
2023-01-10 11:31:49 -0800

    Repetition token 4

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,3 +1,18 @@
 #!/usr/bin/env ruby
-## A regular expression that is matches a given pattern
-#puts ARGV[0].scan(/hbt*n/).join
+### A regular expression that is matches a given pattern
+
+# Reading input arguments
+if ARGV.empty?
+    puts "Please provide a string to match pattern"
+    exit
+end
+
+input_string = ARGV[0]
+
+# matching pattern using regular expression
+# matches = input_string.scan(/hbt*n/)
+# if matches.empty?
+# puts "No match for pattern found in the input string"
+# else
+# puts "The pattern is found in the following positions of the input string: #{matches.join(",")}"
+# end
