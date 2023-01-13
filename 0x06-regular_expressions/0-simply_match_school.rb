25e6f8f38ed6f517251d982d3cb0e7debfa00a19
Kit3l <pchumbakitel@gmail.com>
2023-01-13 02:39:59 -0800

    Simply match school

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,18 +1,3 @@
 #!/usr/bin/env ruby
-### A regular expression that is simply matching School
-
-# Reading input arguments
-if ARGV.empty?
-    puts "Please provide a string to match School"
-    exit
-end
-
-input_string = ARGV[0]
-
-# matching School using regular expression
-matches = input_string.scan(/School/)
-if matches.empty?
-    puts "No match for School found in the input string"
-else
-    puts "The word School is found in the following positions of the input string:
-end
+# A regular expression that is simply matching School
+puts ARGV[0].scan(/School/).join
